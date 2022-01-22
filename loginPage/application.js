7a5095f8e904c9a0034e4a987f7bf4ced1c61df6
Rahul Kurian Jacob <82926759+coderRKJ@users.noreply.github.com>
2022-01-23 03:45:43 +0530

    Converted Watchers into Computed Properties
    Added Bootstrap styling also.

--- loginPage/application.js
+++ loginPage/application.js
@@ -2,80 +2,41 @@ const a = new Vue({
     el: "#app",
     data: {
         pass: "",
-        track : {
-            uppercase: 'rgba(255,0,0,0.4)',
-            lowercase: 'rgba(255,0,0,0.4)',
-            number: 'rgba(255,0,0,0.4)',
-            special: 'rgba(255,0,0,0.4)',
-        },
-        strength: "Please enter a valid password"
-    },
-    methods: {
-            meter: function() {
-                let c = 0;
-                // console.log("check");
-                for (i in this.track) {
-                    if (this.track[i] == 'rgba(0,255,0,0.4)') {
-                        c++;
-                    }
-                }
-                if (this.pass.length > 7) {
-                    c++;
-                }
-
-                if (c == 5) {
-                    this.strength = "Password strength is excellent";
-                }
-                else if (c == 4) {
-                    this.strength = "Password strength is very good";
-                }
-                else if (c == 3) {
-                    this.strength = "Password strength is good";
-                }
-                else if (c == 2) {
-                    this.strength = "Password strength is average";
-                }
-                else if (c == 1) {
-                    this.strength = "Password strength is poor";
-                }
-                else {
-                    this.strength = "Please enter a valid password";
-                }
-            },
-
-            // something: function() {
-            //     console.log("You clicked outside of text input")
-            // }
-
     },
     computed: {
+        uppercase: function () {
+            return this.pass.match('[A-Z]') ? 'alert alert-success': 'alert alert-danger';
+        },
+        lowercase: function () {
+            return this.pass.match('[a-z]') ? 'alert alert-success': 'alert alert-danger';
+        },
+        number: function () {
+            return this.pass.match('[0-9]') ? 'alert alert-success': 'alert alert-danger';
+        },
+        special: function () {
+            return this.pass.match(/[^\w\s]/) ? 'alert alert-success': 'alert alert-danger';
+        },
+        strength: function () {
+            c = 0;
+            c += Number(this.uppercase == 'alert alert-success');
+            c += Number(this.lowercase == 'alert alert-success');
+            c += Number(this.number == 'alert alert-success');
+            c += Number(this.special == 'alert alert-success');
+            c += Number(this.pass.length > 7);
 
-    },
-    watch: {
-        pass(val) {
-
-            this.track.uppercase = 'rgba(255,0,0,0.4)';
-            this.track.lowercase = 'rgba(255,0,0,0.4)';
-            this.track.number = 'rgba(255,0,0,0.4)';
-            this.track.special = 'rgba(255,0,0,0.4)';
-
-            for (let i = 0; i < val.length; i++) {
-                let ch = val.charCodeAt(i);
-
-                if (ch >= 65 && ch <= 90) {
-                    this.track.uppercase = 'rgba(0,255,0,0.4)';
-                }
-                else if (ch >= 97 && ch <= 122) {
-                    this.track.lowercase = 'rgba(0,255,0,0.4)';
-                }
-                else if (ch >= 48 && ch <= 57) {
-                    this.track.number = 'rgba(0,255,0,0.4)';
-                }
-                else if ((ch >= 33 && ch <= 47) || (ch >= 58 && ch <= 64)) {
-                    this.track.special = 'rgba(0,255,0,0.4)';
-                }
-            } 
-            this.meter();
+            if (c == 5) {
+                return `<span class="alert alert-success">Password strength is excellent</span>`;
+            }
+            if (c == 4 || c == 3) {
+                return `<span class="alert alert-primary">Password strength is very good</span>`;
+            }
+            if (c == 2) {
+                return `<span class="alert alert-info">Password strength is average</span>`;
+            }
+            if (c == 1) {
+                return `<span class="alert alert-danger">Password strength is poor</span>`;
+            }
+            return `<span class="alert alert-warning">Please enter a valid passwordr</span>`;
         }
-    }
+    },
 })
